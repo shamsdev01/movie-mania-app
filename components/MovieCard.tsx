dbee189b6ab4845b63d769c13f7cd421fc1c789f
Ajasa Shamsudeen <shamsudeenajasa@gmail.com>
2025-10-21 07:50:29 +0100

    feat: add MovieCard component and implement movie listing with loading and error handling

---
+++ components/MovieCard.tsx
@@ -0,0 +1,37 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+import { icons } from '@/constants/icons';
+
+const MovieCard = ({id, poster_path, title,vote_average,release_date}:Movie) => {
+    console.log(poster_path);
+  return (
+    <Link href={`/movies/${id}`} asChild>
+        <TouchableOpacity className="w-[30%]">
+            <Image
+                 source={{
+                    uri: poster_path
+                    ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                    : 'https://via.placeholder.com/600x400/1a1a1a/ffffff.png',
+                 }}
+                    className="w-full h-52 rounded-lg mb-2"
+                    resizeMode='cover'
+            />
+            <Text className="text-white text-sm font-bold mt-2" numberOfLines={1}>{title}</Text>
+            <View className='flex-row items-center justify-start gap-x-1'>
+                <Image source={icons.star} className="size-4" />
+                <Text className="text-white text-xs font-bold uppercase">{Math.round(vote_average/2)}%</Text>
+                
+            </View>
+            <View className='flex-row items-center justify-between'>
+                <Text className="text-light-300 text-xs font-medium mt-1">{release_date?.split('-')[0]}</Text>  
+                {/* <Text className="text-light-300 text-xs font-medium mt-1">Movie</Text>   */}
+            </View>
+        </TouchableOpacity>
+
+    </Link>
+  )
+
+}
+
+export default MovieCard
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     // Ensure this points to your source code
-    "./app/**/*.{js,tsx,ts,jsx}",
+    "./app/**/*.{js,tsx,ts,jsx}", "./components/**/*.{js,tsx,ts,jsx}"
     // If you use a `src` directory, add: './src/**/*.{js,tsx,ts,jsx}'
     // Do the same with `components`, `hooks`, `styles`, or any other top-level directories
   ],
